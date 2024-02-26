4adff83976441b768e6f15e17dd31f8b4a8a8c25
AbiOnMac <104052078+AbiOnMac@users.noreply.github.com>
2024-02-26 05:58:46 +0000

    removing unwanted spacing and details in seed

--- db/schema.rb
+++ db/schema.rb
@@ -14,23 +14,23 @@
 ActiveRecord::Schema.define(version: 20231211053251) do
 
   create_table "students", force: :cascade do |t|
-    t.string   "first_name",  limit: 255
-    t.string   "middle_name", limit: 255
-    t.string   "last_name",   limit: 255
-    t.string   "email",       limit: 255
-    t.date     "birth_date"
-    t.string   "gender",      limit: 255
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.integer  "title_id",    limit: 4
+    t.string "first_name", limit: 255
+    t.string "middle_name", limit: 255
+    t.string "last_name", limit: 255
+    t.string "email", limit: 255
+    t.date "birth_date"
+    t.string "gender", limit: 255
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "title_id", limit: 4
   end
 
   add_index "students", ["title_id"], name: "index_students_on_title_id", using: :btree
 
   create_table "titles", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string "name", limit: 255
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "students", "titles"
