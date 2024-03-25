eb0811a8cb0dc0981ec54bb4d100019212fa93d9
AbiOnMac <104052078+AbiOnMac@users.noreply.github.com>
2024-03-25 03:29:44 +0000

    Factory bot value updated with some realistic data

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :course do
-    name { "MyString" }
-    description { "MyText" }
+    name { "Bachelor of Computer Science" }
+    description { "Computer science Course" }
     number_of_semesters { 1 }
     start_date { "2024-03-25" }
     end_date { "2024-03-25" }
